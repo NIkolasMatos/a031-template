6c820d421c68a92197f353dd0a8079f35e9aa9b8
NikolasMatos <npequenomatos@gmail.com>
2022-10-11 15:29:13 -0300

    resolvido

---
+++ template/src/components/footer.js
@@ -0,0 +1,12 @@
+import CopyRight from "./copyright"
+import React from "react"
+
+function Footer(){
+    return(
+        <div>
+            <CopyRight/>
+        </div>
+    )
+}
+
+export default Footer
